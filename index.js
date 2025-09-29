2b45640bb8f311677c13ef64c1328100e95dd942
In Hwa Na <n10851879@qut.edu.au>
2025-09-29 17:26:22 +1000

    Merge with AWS S3

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const session = require('express-session')
 const dotenv = require('dotenv')
 const fs = require('fs')
 const path = require('path')
+
+//AWS S3
 const S3 = require("@aws-sdk/client-s3") // AWS S3
+const S3Presigner = require("@aws-sdk/s3-request-presigner");
+const { Upload } = require("@aws-sdk/lib-storage")
+const { PassThrough } = require('stream');
+const { DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const bucketName = 'n10851879-test' // Test Bucket Name
+s3Client = new S3.S3Client({ region: 'ap-southeast-2'})
+
+//AWS Secrets
 const SecretsManager = require("@aws-sdk/client-secrets-manager");
 const { cognitoSignUp } = require("./auth.js")
 const { getSecrets } = require("./secrets.js")
+
 // router for routes// Delete this line maybe...
 
 
@@ -18,10 +28,11 @@ const app = express()
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true })); // To get forms from EJS
 dotenv.config() // Configuratio
+app.use(express.json()) // For parsing json
 
 
 //Multer
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
     destination: (req, file, cb) =>{
         cb(null, 'upload')
     },
@@ -33,7 +44,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage})
 
 //Create uplaod file if doesnt exist
-if(!fs.existsSync("upload")){fs.mkdirSync("upload")}
+if(!fs.existsSync("upload")){fs.mkdirSync("upload")}*/
 
 
 
@@ -45,6 +56,96 @@ app.get('/upload',(req,res) =>{
     res.render("upload")
 })
 
+//S3 Upload
+app.post('/upload',checkauthenticated, async (req,res)=>{
+    // Return Upload Presigned URL
+    const {filename} = req.body
+    //const {filename, contentType} = req.body
+    try {
+        const command = new S3.PutObjectCommand({
+                Bucket: bucketName,
+                Key: filename,
+                //ContentType: contentType
+            });
+        const presignedURL = await S3Presigner.getSignedUrl(s3Client, command, {expiresIn: 3600} );
+        console.log(presignedURL);
+        //console.log("Received:", filename, contentType);
+        res.json({url :presignedURL})
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+// Transcode the video from S3
+app.post('/transcode',checkauthenticated, async (req,res) =>{
+    const {filename} = req.body
+    let transcodedkey = `transcoded${filename}`
+    let response
+
+    // Create and send a command to read an object, Download the video from S3
+    try {
+        response = await s3Client.send(
+            new S3.GetObjectCommand({
+                Bucket: bucketName,
+                Key: filename,
+            }))
+    const video = response.Body
+    const videostream = new PassThrough()
+
+    //Creating Upload, uploading mp4 video
+    const uploads3 = new Upload({
+        client: s3Client,
+        params: {
+            Bucket: bucketName,
+            Key:transcodedkey,
+            Body: videostream,
+            ContentType: 'video/mp4'
+        }
+    })
+
+    // Transcoding Using FFMPEG
+    ffmpeg(video)
+    .outputOptions('-movflags frag_keyframe+empty_moov') // Used because MP4 does not work well with streams
+    .videoCodec('libx264')
+    .format('mp4')
+    .on('start', cmd => console.log('FFmpeg started:', cmd))
+    .on('error', (err) => {
+    console.error('Error:', err.message);
+    res.status(500).send("Transcoding Failed :(")
+    return;
+    })
+    .on('end', ()=>{
+        console.log("Transcoding Complete")
+    })
+    .pipe(videostream, {end: true})
+
+    // Start Uploading
+    await uploads3.done()
+
+    // Create a pre-signed URL for reading an object
+    const command = new S3.GetObjectCommand({
+            Bucket: bucketName,
+            Key: transcodedkey,
+            ResponseContentDisposition: 'attachment; filename="transcodedvideo.mp4"', // Used for directly downloading from presigned URL
+        });
+    const downloadpresignedURL = await S3Presigner.getSignedUrl(s3Client, command, {expiresIn: 3600} );
+    res.json({url :downloadpresignedURL})
+    console.log(downloadpresignedURL)
+
+    // Delete Original Video    
+    const data = await s3Client.send(new DeleteObjectCommand({
+        Bucket: bucketName,
+        Key: filename
+    }));
+    console.log("Success. Object deleted.", data);
+    // Delete Original Video 
+
+    }catch (err) {
+        console.log(err);
+    }
+})
+
+/*
 //Upload Video Transcoding, Used AI to find the ffmpeg setting to create high CPU usage.
 app.post('/upload',upload.single("video"), (req,res)=>{
     const inputpath = req.file.path
@@ -109,7 +210,7 @@ app.get('/download/:filename',(req, res) => {
   const filepath = path.join(__dirname, 'upload', req.params.filename);
   res.download(filepath);
 });
-
+*/
 
 //Login
 app.get('/',(req, res)=>{    
