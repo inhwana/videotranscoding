5c2642d7f83ea6bc57383f1347f302262d0d3e6e
Manny <iamgoice@gmail.com>
2025-10-02 20:35:58 +1000

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ async function bootstrap() {
           .status(403)
           .json({ error: "Video not found or unauthorized" });
       }
-      const storedFileName = videoMetadata.storedFileName;
+      const storedFileName = videoMetadata.storedfileName;
       const transcodedkey = `transcoded${storedFileName}`;
       const response = await s3Client.send(
         new GetObjectCommand({
