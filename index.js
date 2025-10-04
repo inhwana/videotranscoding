900b2088af0fdf52b444cb61f98350538272e11c
Manny <iamgoice@gmail.com>
2025-10-04 21:44:42 +1000

    Update index.js

--- index.js
+++ index.js
@@ -382,7 +382,7 @@ async function bootstrap() {
           .status(404)
           .json({ error: "Video not found or unauthorized" });
 
-      const videoKey = video.storedfilename;
+      const videoKey = video.storedFilename;
       const audioKey = videoKey.replace(/\.[^/.]+$/, ".mp3");
 
       let audioExists = true;
